ed4d75bcf890b77b980eff690b00e83a959c5a51
Kilian <kilian.screnk@sz.de>
2020-04-04 14:54:37 +0200

    pytest

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy and hope its working")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
